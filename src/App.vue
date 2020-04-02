f954ae7019098eb2a44240930eb792465e03fae2
Mathieu Mure <mathieu.mure@gmail.com>
2020-04-02 20:28:01 +0200

    feat(homepage): add style for homepage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,130 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="main">
+    <h1 class="title">Back</h1>
+    <h2 class="subtitle">&vue (ture)</h2>
+    <h3 class="version">{{parseVue(vueVersion)}}</h3>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pakageData from '../package.json'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    vueVersion: pakageData.dependencies.vue
+  }),
+  methods: {
+    parseVue() {
+      if (/^\^?2\./.exec(this.vueVersion)) {
+        return 'II';
+      }
+      if (/^\^?3\./.exec(this.vueVersion)) {
+        return 'III';
+      }
+      return '';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @font-face {
+    font-family: "BTTF";
+    src: url("/BTTF.ttf");
+  }
+
+  body {
+    background-color: black;
+    color: white;
+  }
+
+  @keyframes slideFromRight {
+    from {
+      transform: rotateX(3deg) rotateY(16deg) rotateZ(-7deg) translateX(120vw);
+    }
+    to {
+      transform: rotateX(3deg) rotateY(16deg) rotateZ(-7deg) translateX(20px);
+    }
+  }
+
+  @keyframes slideFromLeft {
+    from {
+      transform: rotateX(3deg) rotateY(16deg) rotateZ(-7deg) translateX(-120vw) translateY(-30px);
+    }
+    to {
+      transform: rotateX(3deg) rotateY(16deg) rotateZ(-7deg) translateX(200px) translateY(-30px);
+    }
+  }
+
+  @keyframes appear {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  .main {
+    width: 100vw;
+    overflow: hidden;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    perspective: 500px;
+  }
+
+  .title, .subtitle, .version {
+    margin: 0;
+    font-family: "BTTF", sans-serif;
+  }
+
+  .title {
+    font-size: 150px;
+    padding-right: 40px;
+    padding-left: 40px;
+    font-weight: bold;
+    background: linear-gradient(to bottom, #B50D13 30%, #F7DC11 70%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+    animation: slideFromRight ease-in-out 2s both;
+
+  }
+  .title:after {
+    content:'<';
+  }
+
+  .subtitle {
+    font-size: 115px;
+    background: linear-gradient(to top, #B50D13 0%, #F7DC11 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: slideFromLeft ease-in-out 2s both 1s;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .version {
+    font-size: 150px;
+    position: absolute;
+    padding-right: 40px;
+    padding-left: 40px;
+    top: calc(50% + 100px);
+    left: calc(50%);
+    background: linear-gradient(to top, #B50D13 0%, #F7DC11 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    transform: rotateX(3deg) rotateY(16deg) rotateZ(-7deg) translateX(200px) translateY(-30px);
+    opacity: 0;
+
+    animation: appear 1s ease-in-out 3s both;
+  }
+  .version:before {
+    content: '>'
+  }
 </style>
