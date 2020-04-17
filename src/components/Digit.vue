d70a7f14502e4a551d8443297ef022ef01022c3d
Mathieu Mure <mathieu.mure@gmail.com>
2020-04-17 20:27:58 +0200

    feat: add digit component

---
+++ src/components/Digit.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div class="line">
+      <div class="tick horizontal" :class="{'active': isTopActive}"></div>
+    </div>
+    <div class="line">
+      <div class="tick vertical" :class="{'active': isTopLeftActive}"></div>
+      <div class="tick vertical" :class="{'active': isTopRightActive}"></div>
+    </div>
+    <div class="line">
+      <div class="tick horizontal" :class="{'active': isMiddleActive}"></div>
+    </div>
+    <div class="line">
+      <div class="tick vertical" :class="{'active': isBottomLeftActive}"></div>
+      <div class="tick vertical" :class="{'active': isBottomRightActive}"></div>
+    </div>
+    <div class="line">
+      <div class="tick horizontal" :class="{'active': isBottomActive}"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Digit',
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    }
+  },
+  computed: {
+    isTopActive () {
+      return [0, 2, 3, 5, 6, 7, 8, 9].includes(this.value)
+    },
+    isTopLeftActive () {
+      return [0, 4, 5, 6, 8, 9].includes(this.value)
+    },
+    isTopRightActive () {
+      return [0, 1, 2, 3, 4, 7, 8, 9].includes(this.value)
+    },
+    isMiddleActive () {
+      return [2, 3, 4, 5, 6, 8, 9].includes(this.value)
+    },
+    isBottomLeftActive () {
+      return [0, 2, 6, 8].includes(this.value)
+    },
+    isBottomRightActive () {
+      return [0, 1, 3, 4, 5, 6, 7, 8, 9].includes(this.value)
+    },
+    isBottomActive () {
+      return [0, 2, 3, 5, 6, 8, 9].includes(this.value)
+    },
+  }
+}
+</script>
+<style scoped="true">
+  .container {
+    display: inline-flex;
+    flex-direction: column;
+  }
+  .line {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 2px;
+  }
+
+  .tick {
+    border-radius: 10%;
+    background-color: #421a00;
+  }
+  .tick.active {
+    background-color: #fa6a01;
+  }
+  .tick.horizontal {
+    height: 5px;
+    width: 40px;
+  }
+  .tick.vertical {
+    width: 5px;
+    height: 40px;
+  }
+</style>
--- src/views/TimeCircuit.vue
+++ src/views/TimeCircuit.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     <h1>Time Circuit</h1>
+    <div class="digit-container">
+      <Digit :value="0"/>
+      <Digit :value="1"/>
+      <Digit :value="2"/>
+      <Digit :value="3"/>
+      <Digit :value="4"/>
+      <Digit :value="5"/>
+      <Digit :value="6"/>
+      <Digit :value="7"/>
+      <Digit :value="8"/>
+      <Digit :value="9"/>
+    </div>
     <button type="button" @click="travels">travel</button>
   </div>
 </template>
 
 <script>
+import Digit from "../components/Digit";
+
 export default {
   name: 'TimeCircuit',
+  components: {Digit},
   methods: {
     travels() {
         this.$router.push({name: 'time-travel'})
@@ -16,4 +31,9 @@ export default {
 }
 </script>
 
-<style scoped="true"></style>
+<style scoped="true">
+  .digit-container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
